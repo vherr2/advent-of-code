3e598e96d0006037879245c2a9ad6a9bbbba427e
vherr2 <vherr2@illinois.edu>
2019-12-01 22:13:54 -0600

    Adds base project for 2015-2018

---
+++ 2015/elixir/test/advent_of_code/day_01_test.exs
@@ -0,0 +1,19 @@
+defmodule AdventOfCode.Day01Test do
+  use ExUnit.Case
+
+  import AdventOfCode.Day01
+
+  test "part1" do
+    input = nil
+    result = part1(input)
+
+    assert result
+  end
+
+  test "part2" do
+    input = nil
+    result = part2(input)
+
+    assert result
+  end
+end
