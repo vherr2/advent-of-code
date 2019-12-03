e636a52cd080e53ae496ec17aa5cba001d16c69e
vherr2 <vherr2@illinois.edu>
2019-12-02 18:36:40 -0600

    Adds solution for 2018d01

--- 2018/elixir/lib/advent_of_code/day_01.ex
+++ 2018/elixir/lib/advent_of_code/day_01.ex
@@ -1,7 +1,24 @@
 defmodule AdventOfCode.Day01 do
   def part1(args) do
+    Enum.sum(args)
   end
 
   def part2(args) do
+    args
+    |> Stream.cycle()
+    |> Enum.reduce_while(%{count: 0, seen: []}, fn freq, acc ->
+      new_freq = acc.count + freq
+
+      if new_freq not in acc.seen do
+        new_acc =
+          acc
+          |> Map.put(:count, new_freq)
+          |> Map.update!(:seen, &([new_freq | &1]))
+
+          {:cont, new_acc}
+        else
+          {:halt, new_freq}
+        end
+      end)
   end
 end
--- 2018/elixir/test/advent_of_code/day_01_test.exs
+++ 2018/elixir/test/advent_of_code/day_01_test.exs
@@ -4,16 +4,16 @@ defmodule AdventOfCode.Day01Test do
   import AdventOfCode.Day01
 
   test "part1" do
-    input = nil
+    input = [+1, -2, +3, +1]
     result = part1(input)
 
-    assert result
+    assert result == 3
   end
 
   test "part2" do
-    input = nil
+    input = [+1, -2, +3, +1]
     result = part2(input)
 
-    assert result
+    assert result == 2
   end
 end
