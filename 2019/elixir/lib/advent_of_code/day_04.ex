b41620227566796fc1867af2400c3281680132b5
vherr2 <vherr2@illinois.edu>
2019-12-04 23:40:43 -0600

    Adds solution for 2019d04

--- 2019/elixir/lib/advent_of_code/day_04.ex
+++ 2019/elixir/lib/advent_of_code/day_04.ex
@@ -1,7 +1,36 @@
 defmodule AdventOfCode.Day04 do
   def part1(args) do
+    filter_func = fn group -> length(group) >= 2 end
+
+    args
+    |> Stream.filter(&monotonic_increase?/1)
+    |> Stream.filter(&two_adjacent?(&1, filter_func))
+    |> Enum.count()
   end
 
   def part2(args) do
+    filter_func = fn group -> length(group) == 2 end
+
+    args
+    |> Stream.filter(&monotonic_increase?/1)
+    |> Stream.filter(&two_adjacent?(&1, filter_func))
+    |> Enum.count()
+  end
+
+  defp monotonic_increase?(number) do
+    number
+    |> to_string()
+    |> String.split("", trim: true)
+    |> Enum.chunk_every(2, 1, :discard)
+    |> Enum.all?(fn [left, right] -> left <= right end)
+  end
+
+  defp two_adjacent?(number, filter_func) do
+    number
+    |> to_string()
+    |> String.split("", trim: true)
+    |> Enum.group_by(& &1)
+    |> Map.values()
+    |> Enum.any?(&filter_func.(&1))
   end
 end
--- 2019/elixir/test/advent_of_code/day_04_test.exs
+++ 2019/elixir/test/advent_of_code/day_04_test.exs
@@ -3,19 +3,17 @@ defmodule AdventOfCode.Day04Test do
 
   import AdventOfCode.Day04
 
-  @tag :skip
   test "part1" do
-    input = nil
+    input = [111_111, 223_450, 123_789]
     result = part1(input)
 
-    assert result
+    assert result == 1
   end
 
-  @tag :skip
   test "part2" do
-    input = nil
+    input = [112_233, 123_444, 111_122]
     result = part2(input)
 
-    assert result
+    assert result == 2
   end
 end
