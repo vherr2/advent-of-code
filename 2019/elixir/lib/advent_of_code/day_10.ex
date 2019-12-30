a999d260b408b9fa78f4be3c8742f777ecfe2405
vherr2 <vherr2@illinois.edu>
2019-12-30 09:30:06 -0600

    feat(day 10 part 1): adds solution for 2019d10p01
    resolves vherr2/advent-of-code#1

--- 2019/elixir/lib/advent_of_code/day_10.ex
+++ 2019/elixir/lib/advent_of_code/day_10.ex
@@ -1,5 +1,52 @@
+defmodule Coordinate do
+  @empty "."
+  @asteroid "#"
+
+  defstruct [:element, :x, :y]
+
+  def empty?(%__MODULE__{element: element}), do: element == @empty
+
+  def asteroid?(%__MODULE__{element: element}), do: element == @asteroid
+
+  def angle(%{x: x, y: y}, %{x: x, y: y}), do: nil
+  def angle(%{x: x, y: ly}, %{x: x, y: ry}), do: {:=, sign(ly, ry), :infinity}
+
+  def angle(left, right) do
+    %{x: lx, y: ly} = left
+    %{x: rx, y: ry} = right
+    dx = lx - rx
+    dy = ly - ry
+
+    {sign(lx, rx), sign(ly, ry), :math.tan(dy / dx)}
+  end
+
+  defp sign(left, right) when left == right, do: :=
+  defp sign(left, right) when left < right, do: :-
+  defp sign(left, right) when left > right, do: :+
+end
+
 defmodule AdventOfCode.Day10 do
   def part1(args) do
+    asteroids =
+      args
+      |> Enum.map(&Enum.with_index/1)
+      |> Enum.with_index()
+      |> Enum.flat_map(fn {xs, y} ->
+        Enum.map(xs, fn {element, x} ->
+          %Coordinate{element: element, x: x, y: y}
+        end)
+      end)
+      |> Enum.filter(&Coordinate.asteroid?/1)
+
+    asteroids
+    |> Enum.map(fn asteroid ->
+      asteroids
+      |> Enum.map(fn dest -> Coordinate.angle(asteroid, dest) end)
+      |> Enum.reject(&is_nil/1)
+      |> Enum.uniq()
+      |> length()
+    end)
+    |> Enum.max()
   end
 
   def part2(args) do
--- 2019/elixir/lib/mix/tasks/d10.p1.ex
+++ 2019/elixir/lib/mix/tasks/d10.p1.ex
@@ -5,7 +5,11 @@ defmodule Mix.Tasks.D10.P1 do
 
   @shortdoc "Day 10 Part 1"
   def run(args) do
-    input = nil
+    input =
+      "priv/day_10.txt"
+      |> File.stream!([:read])
+      |> Stream.map(&String.trim/1)
+      |> Enum.map(&String.split(&1, "", trim: true))
 
     if Enum.member?(args, "-b"),
       do: Benchee.run(%{part_1: fn -> input |> part1() end}),
