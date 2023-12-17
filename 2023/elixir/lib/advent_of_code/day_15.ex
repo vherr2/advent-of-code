a524cb6607cac619c3d543a24f27331eaf7ffc01
vherr2 <vherr2@illinois.edu>
2023-12-17 00:11:59 -0800

    feat(day_15, elixir): adds 2023/15 elixir impl

--- 2023/elixir/lib/advent_of_code/day_15.ex
+++ 2023/elixir/lib/advent_of_code/day_15.ex
@@ -1,7 +1,71 @@
 defmodule AdventOfCode.Day15 do
-  def part1(_args) do
+  def part1(args) do
+    args
+    |> String.trim()
+    |> String.split(",")
+    |> Enum.map(fn seq ->
+      seq
+      |> String.split("")
+      |> Enum.reject(&(&1 == ""))
+      |> Enum.reduce(0, fn char, acc -> parse_char(char, acc) end)
+    end)
+    |> Enum.sum()
   end
 
-  def part2(_args) do
+  def part2(args) do
+    args
+    |> String.trim()
+    |> String.split(",")
+    |> Enum.reduce(List.duplicate([], 256), fn seq, acc ->
+      cond do
+        String.contains?(seq, "-") -> remove_lens(seq, acc)
+        String.contains?(seq, "=") -> replace_lens(seq, acc)
+      end
+    end)
+    |> Enum.with_index(1)
+    |> Enum.reduce(0, fn {box, box_idx}, acc ->
+      box
+      |> Enum.with_index(1)
+      |> Enum.reduce(0, fn {{_, focus}, idx}, acc ->
+        acc + (box_idx * idx * focus)
+      end)
+      |> Kernel.+(acc)
+    end)
+  end
+
+  defp ascii(var) do
+    <<v::utf8>> = var
+
+    v
+  end
+
+  def parse_char(char, acc) do
+    rem((acc + ascii(char)) * 17, 256)
+  end
+
+  defp hash_box(label) do
+    label
+    |> String.split("", trim: true)
+    |> Enum.reduce(0, &parse_char/2)
+  end
+
+  defp remove_lens(seq, acc) do
+    [label, length] = String.split(seq, "-", parts: 2)
+
+    box = hash_box(label)
+
+    List.update_at(acc, box, fn contents ->
+      List.keydelete(contents, label, 0)
+    end)
+  end
+
+  defp replace_lens(seq, acc) do
+    [label, length] = String.split(seq, "=", parts: 2)
+
+    box = hash_box(label)
+
+    List.update_at(acc, box, fn contents ->
+      List.keystore(contents, label, 0, {label, String.to_integer(length)})
+    end)
   end
 end
