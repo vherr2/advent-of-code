d59a307d3e8321769bf67b899ac551408f53a4e5
vherr2 <vherr2@illinois.edu>
2023-12-16 22:18:49 -0800

    feat(day_06, elixir): adds 2023/06 elixir impl

--- 2023/elixir/lib/advent_of_code/day_06.ex
+++ 2023/elixir/lib/advent_of_code/day_06.ex
@@ -1,7 +1,55 @@
 defmodule AdventOfCode.Day06 do
-  def part1(_args) do
+  def part1(args) do
+    race_pairs =
+      args
+      |> String.trim()
+      |> String.split("\n", parts: 2)
+      |> Enum.map(fn part ->
+        part
+        |> String.split(~r/\s/)
+        |> Enum.reject(&(&1 == ""))
+        |> Enum.drop(1)
+        |> Enum.map(&Integer.parse/1)
+        |> Enum.map(&elem(&1, 0))
+      end)
+      |> Enum.zip()
+
+    race_pairs
+    |> Enum.map(fn {time, distance} ->
+      1..time
+      |> Enum.map(fn speed ->
+        speed * (time - speed)
+      end)
+      |> Enum.filter(&(&1 > distance))
+      |> length()
+    end)
+    |> Enum.reduce(1, fn margin, acc ->
+      acc * margin
+    end)
   end
 
-  def part2(_args) do
+  def part2(args) do
+    [time, distance] =
+      args
+      |> String.trim()
+      |> String.split("\n", parts: 2)
+      |> Enum.map(fn part ->
+        part
+        |> String.split(~r/\s/)
+        |> Enum.reject(&(&1 == ""))
+        |> Enum.drop(1)
+        |> Enum.map(&Integer.parse/1)
+        |> Enum.map(&elem(&1, 0))
+        |> Enum.join()
+        |> Integer.parse
+        |> elem(0)
+      end)
+
+    1..time
+    |> Enum.map(fn speed ->
+      speed * (time - speed)
+    end)
+    |> Enum.filter(&(&1 > distance))
+    |> length()
   end
 end
