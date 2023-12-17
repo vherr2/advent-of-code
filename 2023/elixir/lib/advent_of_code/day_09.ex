671ce399f33b6e38a7d310fd89916c51bab3efa8
vherr2 <vherr2@illinois.edu>
2023-12-17 00:11:09 -0800

    feat(day_09, elixir): adds 2023/09 elixir impl

--- 2023/elixir/lib/advent_of_code/day_09.ex
+++ 2023/elixir/lib/advent_of_code/day_09.ex
@@ -1,7 +1,44 @@
 defmodule AdventOfCode.Day09 do
-  def part1(_args) do
+  def part1(args) do
+    args
+    |> parse_args()
+    |> Enum.map(&Enum.reverse/1)
+    |> Enum.map(fn seq -> historic(seq, &Kernel.-/2) end)
+    |> Enum.map(&List.first/1)
+    |> Enum.sum()
   end
 
-  def part2(_args) do
+  def part2(args) do
+    args
+    |> parse_args()
+    |> Enum.map(fn seq -> historic(seq, &Kernel.-/2) end)
+    |> Enum.map(&List.first/1)
+    |> Enum.sum()
+  end
+
+  defp parse_args(args) do
+    args
+    |> String.trim()
+    |> String.split("\n")
+    |> Enum.map(fn line ->
+      line
+      |> String.split(~r/\s/)
+      |> Enum.map(&Integer.parse/1)
+      |> Enum.map(&elem(&1, 0))
+    end)
+  end
+
+  defp historic(sequence, op) do
+    if Enum.all?(sequence, &(&1 == 0)) do
+      [0 | sequence]
+    else
+      extended =
+        sequence
+        |> Enum.chunk_every(2, 1, :discard)
+        |> Enum.map(fn [r, l] -> r - l end)
+        |> historic(op)
+
+      [List.first(extended) + List.first(sequence) | sequence]
+    end
   end
 end
